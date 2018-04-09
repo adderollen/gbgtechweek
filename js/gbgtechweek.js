96f6389bbc45373ca4beadc710550db5b7d9acb7
Andreas Rolén <andreasrolen93@gmail.com>
2018-04-09 09:25:45 +0200

    added dragons den and added version cache refreshing for css

--- js/gbgtechweek.js
+++ js/gbgtechweek.js
@@ -9,12 +9,33 @@ $('.menu a').click(function() {
     return true;
 });
 
+navigator.sayswho= (function(){
+    var ua= navigator.userAgent, tem,
+    M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+    if(/trident/i.test(M[1])){
+        tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+        return 'IE '+(tem[1] || '');
+    }
+    if(M[1]=== 'Chrome'){
+        tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
+        if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+    }
+    M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+    if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+    return M.join(' ');
+})();
+
 $(document).ready(function() {
 	var width = window.innerWidth;
-    $('.logo-row').addClass('logo-row-animate');
-    $('.logo-row > *').addClass('logo-row-content-animate');
-    $('.menu').addClass('menu-animate')
-    $('.page').addClass('visible-page')
+  $('.logo-row').addClass('logo-row-animate');
+  $('.logo-row > *').addClass('logo-row-content-animate');
+  $('.menu').addClass('menu-animate');
+  $('.page').addClass('visible-page');
+
+  if (navigator.sayswho.includes("IE")) {
+    $('.ie-warning').css('display','block');
+  }
+
 })
 
 function postToGoogle(){
